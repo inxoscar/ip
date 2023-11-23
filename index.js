166ea13668705a23d00c3416969ad8c0b30b7eb3
oscar <oscar@inxvn>
2023-11-23 15:32:47 +0700

    feat: update

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const express = require("express");
 // Initialize Express
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Server is ON");
 });
 
 app.get("/ip", function (req, res) {
-  const ipAddress = req.socket.remoteAddress;
+  const ipAddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   res.send({ ip: ipAddress });
 });
 
